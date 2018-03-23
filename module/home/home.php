0d34817d4989dd5ca46003abb86be8400f767ff5
Aby Dzakiyyah <cyberzilla@ymail.com>
2018-03-23 11:03:06 +0800

    Refactor
    [-] Delete All Files

--- module/home/home.php
+++
@@ -1,5 +0,0 @@
-<?php if ( !defined('DYCMSPATH')) exit('No direct script access allowed');?>
-Bismillah, Selamat datang di home page 
-<br/>
-<button class="btn btn-default primary" onclick="postData('session','errornya');">Baca JSON</button>
-<div id="errornya"></div>
--- plugins/font.googleapis/convert.php
+++
@@ -1,6 +0,0 @@
-<?php 
-$filename=$_GET['f'];
-$type = mime_content_type($filename);
-$imgencoded = "data:".$type.";base64,".base64_encode(file_get_contents($filename));
-echo "$imgencoded";
-?>
