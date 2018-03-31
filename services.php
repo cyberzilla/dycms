3a9228ad45c8a388775fe6d3c34c3ec7f1e5c4ed
Aby Dzakiyyah <cyberzilla@ymail.com>
2018-03-31 10:02:07 +0800

    Update 3.1

---
+++ services.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Abu Dzakiyyah
+ * Date: 3/27/2018
+ * Time: 9:23 PM
+ */
+
+$username = $_POST['username'];
+$password = $_POST['password'];
+
+if($username === 'cyberzilla'){
+    if($password===""){
+        $response = array(
+            "namaLengkap"=>"Dedy Miswar",
+            "profilPicture"=>"assets/images/img/user8-128x128.jpg"
+        );
+    }else if($password==='admin'){
+        $response = array("status"=>"login_sukses");
+    }else{
+        $response = array("status"=>"error_login");
+    }
+}else{
+    $response = array("status"=>"error_login");
+}
+echo json_encode($response);
